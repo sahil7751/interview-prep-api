6314c302577a75d9afb1df35fda1495f9e5f492a
Sahil Jirapure <sahiljirapure8@gmail.com>
2026-06-13 14:08:47 +0530

    updated configuration to *

--- src/main/java/com/jobtracker/config/SecurityConfig.java
+++ src/main/java/com/jobtracker/config/SecurityConfig.java
@@ -79,12 +79,9 @@ public class SecurityConfig {
 
         @Bean
         public CorsConfigurationSource corsConfigurationSource() {
-
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(
-            List.of("http://localhost:3000")
-        );
+        configuration.setAllowedOriginPatterns(List.of("*"));
 
         configuration.setAllowedMethods(
             List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
