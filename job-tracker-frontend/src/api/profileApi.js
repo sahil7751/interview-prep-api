f84e92c0e6a7d68c5706902cc064feeb2faf3e13
Sahil Jirapure <sahiljirapure8@gmail.com>
2026-06-28 11:54:04 +0530

    profile picture

--- job-tracker-frontend/src/api/profileApi.js
+++ job-tracker-frontend/src/api/profileApi.js
@@ -6,7 +6,9 @@ export const profileApi = {
     uploadPicture: (form) => API.post('/profile/picture', form, {
         headers: { 'Content-Type': 'multipart/form-data' }
     }),
-    getPictureUrl: () => '/profile/picture',
+    getPictureUrl: (userId, bust) =>
+        `http://localhost:8081/api/v1/profile/picture?t=${bust || Date.now()}`,
     changePassword: (data) => API.put('/profile/password', data),
     getCompletion: () => API.get('/profile/completion'),
+    reviewProfile: () => API.post('/profile/review'),
 };
