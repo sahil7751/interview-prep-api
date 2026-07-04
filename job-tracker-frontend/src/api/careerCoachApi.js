1612ee4fd00a8fd88e23867e1e84e327af98ddd8
Sahil Jirapure <sahiljirapure8@gmail.com>
2026-07-04 12:00:39 +0530

    updated

--- job-tracker-frontend/src/api/careerCoachApi.js
+++ job-tracker-frontend/src/api/careerCoachApi.js
@@ -2,4 +2,6 @@ import API from './axios';
 
 export const careerCoachApi = {
     chat: (data) => API.post('/career-coach/chat', data),
+    dailyInsight: () => API.get('/career-coach/daily-insight'),
+    weeklyReview: () => API.get('/career-coach/weekly-review'),
 };
